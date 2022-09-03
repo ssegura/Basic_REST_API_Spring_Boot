04eaf7a19c0ba07d1b170808c7144a34837c547f
Sergio Segura <sergiosegura@us.es>
2022-09-03 18:48:51 +0200

    Small format change

--- src/main/java/score/aiss/streamsschedule/model/LiveStream.java
+++ src/main/java/score/aiss/streamsschedule/model/LiveStream.java
@@ -3,7 +3,7 @@ package score.aiss.streamsschedule.model;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
-public record LiveStream(
+public record LiveStream (
         String id,
         @NotEmpty(message = "Stream title is required")
         String title,
